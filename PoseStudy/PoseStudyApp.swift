861a8666b828de06137c968ff863288e59344314
Franziska Lang <franziska@langfeldafing.de>
2020-11-25 12:35:44 +0100

    Initial Commit

---
+++ PoseStudy/PoseStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoseStudyApp.swift
+//  PoseStudy
+//
+//  Created by Franziska Lang on 25.11.20.
+//
+
+import SwiftUI
+
+@main
+struct PoseStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
